8cedf96d118f3cfa546d0af05ccd7d71867e52b2
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2018-11-12 10:05:02 +0100

    repair test

--- cmd/metal-api/internal/utils/jwt/jwt_test.go
+++ cmd/metal-api/internal/utils/jwt/jwt_test.go
@@ -3,7 +3,7 @@ package jwt
 import (
 	"testing"
 
-	"git.f-i-ts.de/cloud-native/maas/metal-api/pkg/metal"
+	"git.f-i-ts.de/cloud-native/maas/metal-api/metal"
 )
 
 func TestSerialization(t *testing.T) {
