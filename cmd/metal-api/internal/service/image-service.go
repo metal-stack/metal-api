4c91115ba6c34b715b27eae7501099b32d250d2a
Gerrit <tirreg91@googlemail.com>
2018-11-29 14:22:30 +0100

    Update spec.

--- cmd/metal-api/internal/service/image-service.go
+++ cmd/metal-api/internal/service/image-service.go
@@ -117,6 +117,7 @@ func (ir imageResource) createImage(request *restful.Request, response *restful.
 	s.Changed = s.Created
 	img, err := ir.ds.CreateImage(&s)
 	if err != nil {
+		// FIXME: Return statement missing?
 		sendError(ir.log, response, "createImage", http.StatusInternalServerError, err)
 	} else {
 		response.WriteHeaderAndEntity(http.StatusCreated, img)
