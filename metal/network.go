0ca7145c4d0e85144dc671e6a5d8de26745b6a63
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2018-12-03 10:21:20 +0000

    the neighours are still nic's

--- metal/network.go
+++ metal/network.go
@@ -2,12 +2,7 @@ package metal
 
 // Nic information.
 type Nic struct {
-	MacAddress string     `json:"mac"  description:"the mac address of this network interface" rethinkdb:"macAddress"`
-	Name       string     `json:"name"  description:"the name of this network interface" rethinkdb:"name"`
-	Neighbors  []Neighbor `json:"neighbors" description:"the neighbors visible to this network interface"`
-}
-
-// Neighbor of a nic.
-type Neighbor struct {
-	MacAddress string `json:"mac"  description:"the mac address of the neighbor interface" rethinkdb:"macAddress"`
+	MacAddress string `json:"mac"  description:"the mac address of this network interface" rethinkdb:"macAddress"`
+	Name       string `json:"name"  description:"the name of this network interface" rethinkdb:"name"`
+	Neighbors  []Nic  `json:"neighbors" description:"the neighbors visible to this network interface"`
 }
