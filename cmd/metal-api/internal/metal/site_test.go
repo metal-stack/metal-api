50fb3b863a09d138d9e83e03dae94607d3713e8d
Ferdinand Eckhard <Ferdinand.Eckard@f-i-ts.de>
2019-01-11 14:30:54 +0100

    replced generated tests with static tests.

--- cmd/metal-api/internal/metal/site_test.go
+++ cmd/metal-api/internal/metal/site_test.go
@@ -1,75 +1,27 @@
 package metal
 
-/*
-func getMap(x int, y int, baseArray []Base) map[string]Site {
-	// Returns a Site Map
-	return map[string]Site{baseArray[x].ID: Site{baseArray[x]}, baseArray[y].ID: Site{baseArray[y]}}
-}
-
-func getAllTestStructs(baseArray []Base, siteArray []Site) []struct {
-	name  string
-	sites Sites
-	want  SiteMap
-} {
-	structArray := make([]struct {
-		name  string
-		sites Sites
-		want  SiteMap
-	}, len(siteArray)*len(baseArray))
-	index := 0
-	for i := 0; i < len(baseArray); i++ {
-		for j := 0; j < len(siteArray); j++ {
-			structArray[index] = struct {
-				name  string
-				sites Sites
-				want  SiteMap
-			}{
-
-				name: "real live data",
-				sites: Sites{
-					siteArray[i],
-					siteArray[j],
-				},
-				want: getMap(i, j, baseArray),
-			}
-			index++
-		}
-	}
-	return structArray
-}
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSites_ByID(t *testing.T) {
-
-	// Definition of Base names (can be increased, all test are generated from this array automaticaly)
-	var nameArray = []string{"micro", "tiny", "microAndTiny"}
-	length := len(nameArray)
-
-	// Create Base Array
-	baseArray := make([]Base, length)
-	for i := 0; i < length; i++ {
-		baseArray[i] = Base{
-			Name: nameArray[i],
-			ID:   "test",
-		}
+	tests := []struct {
+		name string
+		sz   Sites
+		want SiteMap
+	}{
+		{
+			name: "TestSites_ByID Test 1",
+			sz:   TestSites,
+			want: map[string]Site{TestSites[0].ID: TestSites[0], TestSites[1].ID: TestSites[1], TestSites[2].ID: TestSites[2]},
+		},
 	}
-
-	// Create Site Array
-	siteArray := make([]Site, length)
-	for i := 0; i < length; i++ {
-		siteArray[i] = Site{
-			Base: baseArray[i],
-		}
-	}
-
-	// Create all Test Data
-	tests := getAllTestStructs(baseArray, siteArray)
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.sites.ByID(); !reflect.DeepEqual(got, tt.want) {
+			if got := tt.sz.ByID(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Sites.ByID() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-*/
