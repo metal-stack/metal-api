7a8dd465774b738ea09590819a17fd6510db4a76
Sandro Koll <sandro.koll@f-i-ts.de>
2019-10-02 17:37:17 +0200

    Cleanup

--- test/docker-compose/entrypoint.sh
+++ test/docker-compose/entrypoint.sh
@@ -11,7 +11,8 @@ metalctl partition apply -f /masterdata/partitions.yaml
 metal-api \
   --db-addr rethinkdb-rethinkdb-proxy:28015 \
   --hmac-admin-lifetime 0 \
-  --nsqd-https-addr metal-control-plane-nsqd:4152 \
+  --nsqd-tcp-addr metal-control-plane-nsqd:4150 \
+  --nsqd-rest-endpoint metal-control-plane-nsqd:4152 \
   --ipam-db-addr metal-control-plane-postgres \
   --ipam-db-password password \
   --ipam-db-user ipam \
