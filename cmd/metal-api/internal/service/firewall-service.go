92e5f98aebf20e8aec42964a19ace8f0b8991660
Sandro Koll <sandro.koll@f-i-ts.de>
2020-06-24 12:04:55 +0200

    use go-restful-openapi/v2. This happened during sync with master...

--- cmd/metal-api/internal/service/firewall-service.go
+++ cmd/metal-api/internal/service/firewall-service.go
@@ -17,7 +17,7 @@ import (
 	v1 "github.com/metal-stack/metal-api/cmd/metal-api/internal/service/v1"
 	"github.com/metal-stack/metal-api/cmd/metal-api/internal/utils"
 
-	restfulspec "github.com/emicklei/go-restful-openapi"
+	restfulspec "github.com/emicklei/go-restful-openapi/v2"
 	restful "github.com/emicklei/go-restful/v3"
 	"github.com/metal-stack/metal-lib/bus"
 )
