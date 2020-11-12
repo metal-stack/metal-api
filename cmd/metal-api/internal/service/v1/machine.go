1d41e18744e3221d102c7aba55c05f81e3f07911
Gerrit <tirreg91@googlemail.com>
2020-11-12 10:19:48 +0100

    Don't return None block for devices but empty array.

--- cmd/metal-api/internal/service/v1/machine.go
+++ cmd/metal-api/internal/service/v1/machine.go
@@ -273,7 +273,7 @@ func NewMetalMachineHardware(r *MachineHardwareExtended) metal.MachineHardware {
 		}
 		nics = append(nics, nic)
 	}
-	var disks []metal.BlockDevice
+	disks := []metal.BlockDevice{}
 	for _, d := range r.Disks {
 		disk := metal.BlockDevice{
 			Name:    d.Name,
