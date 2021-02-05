a775138c4bc848ac2253de972c4f0cbfc8cdb7d4
Gerrit <Gerrit91@users.noreply.github.com>
2021-02-05 11:39:40 +0100

    Make IPs searchable by name. (#163)

--- cmd/metal-api/internal/datastore/ip.go
+++ cmd/metal-api/internal/datastore/ip.go
@@ -12,6 +12,7 @@ import (
 // IPSearchQuery can be used to search networks.
 type IPSearchQuery struct {
 	IPAddress        *string  `json:"ipaddress" modelDescription:"an ip address that can be attached to a machine" description:"the address (ipv4 or ipv6) of this ip" optional:"true"`
+	Name             *string  `json:"name" description:"the name of the ip address" optional:"true"`
 	ParentPrefixCidr *string  `json:"networkprefix" description:"the prefix of the network this ip address belongs to" optional:"true"`
 	NetworkID        *string  `json:"networkid" description:"the network this ip allocate request address belongs to" optional:"true"`
 	Tags             []string `json:"tags" description:"the tags that are assigned to this ip address" optional:"true"`
@@ -27,6 +28,12 @@ func (p *IPSearchQuery) generateTerm(rs *RethinkStore) *r.Term {
 	if p.IPAddress != nil {
 		q = q.Filter(func(row r.Term) r.Term {
 			return row.Field("id").Eq(*p.IPAddress)
+		})
+	}
+
+	if p.Name != nil {
+		q = q.Filter(func(row r.Term) r.Term {
+			return row.Field("name").Eq(*p.Name)
 		})
 	}
 
