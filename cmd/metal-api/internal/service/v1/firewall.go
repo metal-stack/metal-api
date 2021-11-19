0eedb732f779895946bfd872396be1260d5f7761
Stefan Majer <stefan.majer@f-i-ts.de>
2021-11-19 09:56:06 +0100

    HA is unused

--- cmd/metal-api/internal/service/v1/firewall.go
+++ cmd/metal-api/internal/service/v1/firewall.go
@@ -2,6 +2,7 @@ package v1
 
 type FirewallCreateRequest struct {
 	MachineAllocateRequest
+	// FIXME remove this property, is nowhere used
 	HA *bool `json:"ha" description:"if set to true, this firewall is set up in a High Available manner" optional:"true"`
 }
 
