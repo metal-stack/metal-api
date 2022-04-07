4885c78ae6a2e6e3a0b9fdf02c429c18fd54d151
Gerrit <info@gerritschwerthelm.de>
2022-04-07 11:05:39 +0200

    Use extra short timeout for health check.

--- cmd/metal-api/internal/datastore/health.go
+++ cmd/metal-api/internal/datastore/health.go
@@ -2,6 +2,7 @@ package datastore
 
 import (
 	"context"
+	"time"
 
 	"github.com/metal-stack/metal-lib/rest"
 	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
@@ -13,13 +14,16 @@ func (rs *RethinkStore) ServiceName() string {
 
 // Check implements the health interface and tests if the database is healthy.
 func (rs *RethinkStore) Check(ctx context.Context) (rest.HealthStatus, error) {
+	healthCtx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
 	t := r.Branch(
 		rs.db().TableList().SetIntersection(r.Expr(tables)).Count().Eq(len(tables)),
 		r.Expr(true),
 		r.Error("required tables are missing"),
 	)
 
-	err := t.Exec(rs.session, r.ExecOpts{Context: ctx})
+	err := t.Exec(rs.session, r.ExecOpts{Context: healthCtx})
 	if err != nil {
 		return rest.HealthStatusUnhealthy, err
 	}
