306899e7b35a77a99930738b4483353aeb312823
Stefan Majer <stefan.majer@f-i-ts.de>
2022-05-02 08:01:44 +0200

    One events endpoint for single and batch processing

--- cmd/metal-api/internal/grpc/event-service.go
+++ cmd/metal-api/internal/grpc/event-service.go
@@ -2,11 +2,11 @@ package grpc
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/metal-stack/metal-api/cmd/metal-api/internal/metal"
 	v1 "github.com/metal-stack/metal-api/pkg/api/v1"
+	"go.uber.org/multierr"
 	"go.uber.org/zap"
 )
 
@@ -26,32 +26,52 @@ func (e *EventService) Send(ctx context.Context, req *v1.EventServiceSendRequest
 	if req == nil {
 		return nil, fmt.Errorf("no event send")
 	}
-	m, err := e.ds.FindMachineByID(req.MachineId)
-	if err != nil && !metal.IsNotFound(err) {
-		return nil, err
-	}
 
-	// an event can actually create an empty machine. This enables us to also catch the very first PXE Booting event
-	// in a machine lifecycle
-	if m == nil {
-		m = &metal.Machine{
-			Base: metal.Base{
-				ID: req.MachineId,
-			},
+	failed := []string{}
+	processed := uint64(0)
+	var processErr error
+	for machineID, event := range req.Events {
+
+		m, err := e.ds.FindMachineByID(machineID)
+		if err != nil && !metal.IsNotFound(err) {
+			processErr = multierr.Append(processErr, fmt.Errorf("machine with ID:%s not found %w", machineID, err))
+			failed = append(failed, machineID)
+			continue
 		}
-		err = e.ds.CreateMachine(m)
+
+		// an event can actually create an empty machine. This enables us to also catch the very first PXE Booting event
+		// in a machine lifecycle
+		if m == nil {
+			m = &metal.Machine{
+				Base: metal.Base{
+					ID: machineID,
+				},
+			}
+			err = e.ds.CreateMachine(m)
+			if err != nil {
+				processErr = multierr.Append(processErr, err)
+				failed = append(failed, machineID)
+				continue
+			}
+		}
+
+		ok := metal.AllProvisioningEventTypes[metal.ProvisioningEventType(event.Event)]
+		if !ok {
+			processErr = multierr.Append(processErr, err)
+			failed = append(failed, machineID)
+			continue
+		}
+		_, err = e.ds.ProvisioningEventForMachine(machineID, event.Event, event.Message)
 		if err != nil {
-			return nil, err
+			processErr = multierr.Append(processErr, err)
+			failed = append(failed, machineID)
+			continue
 		}
+		processed++
 	}
 
-	ok := metal.AllProvisioningEventTypes[metal.ProvisioningEventType(req.Event)]
-	if !ok {
-		return nil, errors.New("unknown provisioning event")
-	}
-	_, err = e.ds.ProvisioningEventForMachine(req.MachineId, req.Event, req.Message)
-	if err != nil {
-		return nil, err
-	}
-	return &v1.EventServiceSendResponse{}, nil
+	return &v1.EventServiceSendResponse{
+		Events: processed,
+		Failed: failed,
+	}, processErr
 }
