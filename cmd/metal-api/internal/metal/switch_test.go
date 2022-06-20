d86e7b40b45725e94a37d4642e4141045d628eb8
Robert Volkmann <robert.volkmann@x-cellent.com>
2022-06-20 14:47:17 +0200

    Add test for ByNicName

--- cmd/metal-api/internal/metal/switch_test.go
+++ cmd/metal-api/internal/metal/switch_test.go
@@ -47,3 +47,50 @@ func TestSwitch_ConnectMachine(t *testing.T) {
 		t.Errorf("got: %v, want:%v", s.MachineConnections, want)
 	}
 }
+
+func TestConnectionMap_ByNicName(t *testing.T) {
+	tests := []struct {
+		name    string
+		c       ConnectionMap
+		want    map[string]Connection
+		wantErr bool
+	}{
+		{
+			"duplicate connections throws error",
+			ConnectionMap{
+				"machine-1": []Connection{{Nic: Nic{Name: "duplicate"}}},
+				"machine-2": []Connection{{Nic: Nic{Name: "duplicate"}}},
+			},
+			nil,
+			true,
+		},
+		{
+			"happy case",
+			ConnectionMap{
+				"machine-1": []Connection{
+					{Nic: Nic{Name: "eth0"}},
+					{Nic: Nic{Name: "eth1", MacAddress: "I will be preserved"}},
+				},
+				"machine-2": []Connection{{Nic: Nic{Name: "eth2"}}},
+			},
+			map[string]Connection{
+				"eth0": {Nic: Nic{Name: "eth0"}},
+				"eth1": {Nic: Nic{Name: "eth1", MacAddress: "I will be preserved"}},
+				"eth2": {Nic: Nic{Name: "eth2"}},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.c.ByNicName()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ByNicName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ByNicName() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
