a6a8fd6c1e34a108f20d0b6aa4d6305b03849e53
Robert Volkmann <robert.volkmann@x-cellent.com>
2022-06-20 14:34:35 +0200

    Refactor TestSwitch_ConnectMachine

--- cmd/metal-api/internal/metal/switch_test.go
+++ cmd/metal-api/internal/metal/switch_test.go
@@ -5,110 +5,45 @@ import (
 	"testing"
 )
 
-// Gerrit and me implemented that monster in a one shot which worked.
-
-func TestSwitch_ConnectMachine2(t *testing.T) {
-	type fields struct {
-		ID                 string
-		Nics               []Nic
-		MachineConnections ConnectionMap
-		PartitionID        string
-		RackID             string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		machine *Machine
-	}{
-		// Test Data Array (Only 1 Value):
-		{
-			name: "simple connection",
-			fields: fields{
-				ID: "switch-1",
-				Nics: []Nic{
-					{
-						Name:       "eth0",
-						MacAddress: "00:11:11",
-					},
-					{
-						Name:       "swp1",
-						MacAddress: "11:11:11",
-					},
-					{
-						Name:       "swp2",
-						MacAddress: "22:11:11",
-					},
+func TestSwitch_ConnectMachine(t *testing.T) {
+	swp1 := Nic{Name: "swp1", MacAddress: "11:11:11"}
+	swp2 := Nic{Name: "swp2", MacAddress: "22:11:11"}
+	someNeighbor := Nic{MacAddress: "ignored"}
+	machineId := "machine-1"
+	given := &Machine{
+		Base: Base{ID: machineId},
+		Hardware: MachineHardware{
+			Nics: []Nic{
+				{
+					Name:      "eth0",
+					Neighbors: []Nic{swp1, someNeighbor},
 				},
-				PartitionID: "nbg1",
-				RackID:      "rack1",
-				MachineConnections: ConnectionMap{
-					"machine-1": []Connection{
-						{
-							Nic: Nic{
-								Name:       "swp1",
-								MacAddress: "11:11:11",
-							},
-							MachineID: "machine-1",
-						},
-						{
-							Nic: Nic{
-								Name:       "swp2",
-								MacAddress: "22:11:11",
-							},
-							MachineID: "machine-1",
-						},
-					},
-				},
-			},
-			machine: &Machine{
-				Base: Base{
-					ID: "machine-1",
-				},
-				Hardware: MachineHardware{
-					Nics: []Nic{
-						{
-							Name: "eth0",
-							Neighbors: []Nic{
-								{
-									MacAddress: "11:11:11",
-								},
-								{
-									MacAddress: "11:11:12",
-								},
-							},
-						},
-						{
-							Name: "eth1",
-							Neighbors: []Nic{
-								{
-									MacAddress: "22:11:11",
-								},
-								{
-									MacAddress: "11:11:13",
-								},
-							},
-						},
-					},
+				{
+					Name:      "eth1",
+					Neighbors: []Nic{swp2, someNeighbor},
 				},
 			},
 		},
 	}
-	for i := range tests {
-		tt := tests[i]
-		t.Run(tt.name, func(t *testing.T) {
-			s := Switch{
-				Base: Base{
-					ID: tt.fields.ID,
-				},
-				RackID:             tt.fields.RackID,
-				Nics:               tt.fields.Nics,
-				PartitionID:        tt.fields.PartitionID,
-				MachineConnections: ConnectionMap{},
-			}
-			s.ConnectMachine(tt.machine)
-			if !reflect.DeepEqual(s.MachineConnections, tt.fields.MachineConnections) {
-				t.Errorf("expected:%v, got:%v", s.MachineConnections, tt.fields.MachineConnections)
-			}
-		})
+	want := ConnectionMap{
+		machineId: []Connection{
+			{MachineID: machineId, Nic: swp1},
+			{MachineID: machineId, Nic: swp2},
+		},
+	}
+
+	s := Switch{
+		Nics: []Nic{
+			{Name: "eth0", MacAddress: "00:11:11"},
+			swp1,
+			swp2,
+		},
+		MachineConnections: ConnectionMap{},
+	}
+
+	s.ConnectMachine(given)
+
+	if !reflect.DeepEqual(s.MachineConnections, want) {
+		t.Errorf("got: %v, want:%v", s.MachineConnections, want)
 	}
 }
