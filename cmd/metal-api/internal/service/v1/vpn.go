7fdafae56628dd8c969503f92cf46d80dd784631
Grigoriy Mikhalkin <grigoriymikhalkin@gmail.com>
2022-09-02 14:53:42 +0200

    change vpn auth key method to POST

--- cmd/metal-api/internal/service/v1/vpn.go
+++ cmd/metal-api/internal/service/v1/vpn.go
@@ -4,3 +4,7 @@ type VPNResponse struct {
 	Address string `json:"address" description:"address of VPN's control plane"`
 	AuthKey string `json:"auth_key" description:"auth key to connect to the VPN"`
 }
+
+type VPNRequest struct {
+	Pid string `json:"pid" description:"project ID"`
+}
